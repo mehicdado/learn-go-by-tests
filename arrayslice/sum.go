45a6b6d0958b59392c4af14301f084aa100eb5b7
Damir Mehic <damir.mehic@legal.one>
2020-01-11 00:07:35 +0100

    Operations with slices and arrays. Sum of slices.

--- arrayslice/sum.go
+++ arrayslice/sum.go
@@ -1,6 +1,6 @@
 package arrayslice
 
-//Sum function return sum of elements of given array
+//Sum function return sum of elements of given array.
 func Sum(numbers []int) int {
 	sum := 0
 	for _, value := range numbers {
@@ -8,4 +8,30 @@ func Sum(numbers []int) int {
 	}
 
 	return sum
+}
+
+//SumAll will sum elements of each given slice and append results to new slice.
+func SumAll(numbersToSum ...[]int) (sums []int) {
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+//SumAllTails will sum elements for each given slice after excluding first element and
+//append the results to new slice
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return
 }
--- arrayslice/sum_test.go
+++ arrayslice/sum_test.go
@@ -1,6 +1,9 @@
 package arrayslice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -14,5 +17,37 @@ func TestSum(t *testing.T) {
 			t.Errorf("expected %d but got %d given, %v", expected, got, numbers)
 		}
 	})
+}
 
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{3, 9})
+	expected := []int{3, 12}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("expected %v but got %v", expected, got)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	assertSums := func(t *testing.T, got, expected []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("expected %v but got %v", expected, got)
+		}
+	}
+
+	t.Run("make sum of some slice tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 2}, []int{1, 9})
+		expected := []int{4, 9}
+
+		assertSums(t, got, expected)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 9})
+		expected := []int{0, 9}
+
+		assertSums(t, got, expected)
+	})
 }
